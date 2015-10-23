4d4cb41d531602a3094efb3b6ed4f80c0031f998
s.kamardin <s.kamardin@corp.mail.ru>
2015-10-23 22:29:55 +0300

    Preparing for udp

---
+++ test/unit/client/udp.js
@@ -0,0 +1,18 @@
+var UDPClient = require("../../lib/client/udp.js");
+var sinon = require("sinon");
+
+describe("UDPClient", function() {
+
+	describe("constructor", function() {
+
+	});
+
+	describe("injectors", function() {
+
+	});
+
+	describe("instance", function() {
+
+	});
+
+});
